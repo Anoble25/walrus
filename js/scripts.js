a7892012a3fa4c0f142523d1df5059f944481ef2
Alex Noble and Kristi Hwang <kristi@>
2018-03-20 09:16:12 -0700

    add slide and fade effects

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,8 @@ $(document).ready(function() {
   $(".clickable").click(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
+    $("#f1").fadeToggle();
+    $("#s1").slideToggle();
+
   });
 });
